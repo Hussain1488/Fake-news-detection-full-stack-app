b29756245463e809a8c7c4e4ee5aad18d54fb940
Hussain <hedayati1488@gmail.com>
2026-06-26 17:37:48 +0200

    Add Retriever class and test for embedding-based document retrieval

---
+++ Backend/app/rag/retriever.py
@@ -0,0 +1,26 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+
+
+class Retriever:
+  def __init__(self, k: int = 3):
+    self.embedding = HuggingFaceEmbeddings(
+      model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    self.vector_store = Chroma(
+      collection_name="thesis",
+      embedding_function=self.embedding,
+      persist_directory="app/resources/vectorstore"
+    )
+
+    self.retriever = self.vector_store.as_retriever(
+      search_kwargs={"k": k}
+    )
+
+  def retrieve(self, query: str):
+    return self.retriever.invoke(query)
+
+
+retriever = Retriever()
+
--- Backend/app/rag/test_retriever.py
+++ Backend/app/rag/test_retriever.py
@@ -2,21 +2,24 @@ from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 
 
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+def test_retriever():
+  embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
-vector_store = Chroma(
-    collection_name="fake_news_collection",
-    embedding_function=embeddings,
-    persist_directory="app/resources/chroma_db"
-)
+  vector_store = Chroma(
+      collection_name="thesis",
+      embedding_function=embeddings,
+      persist_directory="app/resources/vectorstore"
+  )
 
-retriever = vector_store.as_retriever(
-  search_kwargs={"k": 3}
-)
-
-
-docs = retriever.invoke("What is fake news?")
+  retriever = vector_store.as_retriever(
+    search_kwargs={"k": 3}
+  )
 
 
-for i, doc in enumerate(docs):
-    print(f"Document {i+1}:\n{doc.page_content}\n")
+  docs = retriever.invoke("who is the author of this thesis?")
+
+
+  for i, doc in enumerate(docs):
+      print(f"Document {i+1}:\n{doc.page_content}\n")
+
+test_retriever()
